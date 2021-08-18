52b91d1c257f943f68ca8933b7101c1c03a5368a
Koki Kobayashi <androidvboxtest5@gmail.com>
2021-08-18 20:21:47 +0900

    Update scripts.

--- .zsh/tmux.zsh
+++ .zsh/tmux.zsh
@@ -4,7 +4,7 @@ function is_screen_running() { [ ! -z "$STY" ]; }
 function is_tmux_runnning() { [ ! -z "$TMUX" ]; }
 function is_screen_or_tmux_running() { is_screen_running || is_tmux_runnning; }
 function shell_has_started_interactively() { [ ! -z "$PS1" ]; }
-function is_ssh_running() { [ ! -z "$SSH_CLIENT" ]; }
+function is_ssh_running() { [ ! -z "$SSH_CONNECTION" ]; }
 
 function tmux_automatically_attach_session()
 {
