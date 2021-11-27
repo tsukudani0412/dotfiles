350b4da7dc971a46072b0ef5c413eb5c9bcf6dec
Tsukudani <androidvboxtest5@gmail.com>
2021-11-28 02:28:20 +0900

    Update deploy.sh
    fix zinit repository

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 set -eu
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+yes | sh -c "$(curl -fsSL https://git.io/zinit-install)"
 zsh -i -c "source ~/.zsh/zplugin.zsh"
 curl -fsLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
